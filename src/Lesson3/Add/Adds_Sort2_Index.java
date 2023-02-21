098d1e4fd14fc9d53c3e520e550168f36875ca1c
5691100 <5691100@gmail.com>
2023-02-21 23:31:12 +0300

    Adds_Sort2_index_finish

--- src/Lesson3/Add/Adds_Sort2_Index.java
+++ src/Lesson3/Add/Adds_Sort2_Index.java
@@ -1,9 +1,12 @@
 package Lesson3.Add;
 
+//Сгененрировать 2 массива случайных чисел разного размера. Смержить их в третий, при этом сортируя по убыванию
+
+
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Adds_Sort2_Index_unfinish {
+public class Adds_Sort2_Index {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -31,20 +34,37 @@ public class Adds_Sort2_Index_unfinish {
         int minMin = Integer.MAX_VALUE;
         int counter1 = 0;
         int counter2 = 0;
-        while (i < number1 + number2) {
+        int check1 = 0;
+        int check2 = 0;
+        while ((check1 == 0) && (check2 == 0)) {
             if (array2[counter2] >= array1[counter1]) {
-                    minMin = array1[counter1];
-                    if (counter1 != number1 - 1) {
-                        counter1++;
-                    }
+                minMin = array1[counter1];
+                if (counter1 != number1 - 1) {
+                    counter1++;
                 } else {
-                    minMin = array2[counter2];
-                    if (counter2 != number2 - 1) {
-                        counter2++;
-                    }
+                    check1 = 1;
                 }
+            } else {
+                minMin = array2[counter2];
+                if (counter2 != number2 - 1) {
+                    counter2++;
+                } else {
+                    check2 = 1;
+                }
+            }
             array3[i] = minMin;
             i++;
+        }
+        if (check1 == 1) {
+            for (; i < number1 + number2; i++) {
+                array3[i] = array2[counter2];
+                counter2++;
+            }
+        } else {
+            for (; i < number1 + number2; i++) {
+                array3[i] = array1[counter1];
+                counter1++;
+            }
         }
         System.out.println("Array Merged: " + Arrays.toString(array3));
     }
