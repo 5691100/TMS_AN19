37d282e4c939eef637170a1f96647fb7d7b1f0e2
5691100 <5691100@gmail.com>
2023-02-22 10:11:43 +0300

    L3_Adds_Sort2_index_final

--- src/Lesson3/Add/Adds_Sort2_Index.java
+++ src/Lesson3/Add/Adds_Sort2_Index.java
@@ -4,6 +4,8 @@ package Lesson3.Add;
 
 
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Adds_Sort2_Index {
@@ -14,45 +16,45 @@ public class Adds_Sort2_Index {
         int number1 = sc.nextInt();
         System.out.println("Please enter the number of elements in Array One:");
         int number2 = sc.nextInt();
-        int[] array1 = new int[number1];
+        Integer [] array1 = new Integer[number1];
         for (int i = 0; i < number1; i++) {
             array1[i] = (int) (Math.random() * 100);
         }
-        int[] array2 = new int[number2];
+        Integer [] array2 = new Integer [number2];
         for (int i = 0; i < number2; i++) {
             array2[i] = (int) (Math.random() * 100);
         }
         System.out.println("Array One: " + Arrays.toString(array1));
         System.out.println("Array Two: " + Arrays.toString(array2));
 
-        Arrays.sort(array1);
-        Arrays.sort(array2);
+        Arrays.sort(array1, Collections.reverseOrder());
+        Arrays.sort(array2, Collections.reverseOrder());
         System.out.println("Array One New: " + Arrays.toString(array1));
         System.out.println("Array Two New: " + Arrays.toString(array2));
         int[] array3 = new int[number1 + number2];
         int i = 0;
-        int minMin = Integer.MAX_VALUE;
+        int maxMax = 0;
         int counter1 = 0;
         int counter2 = 0;
         int check1 = 0;
         int check2 = 0;
         while ((check1 == 0) && (check2 == 0)) {
-            if (array2[counter2] >= array1[counter1]) {
-                minMin = array1[counter1];
+            if (array2[counter2] <= array1[counter1]) {
+                maxMax = array1[counter1];
                 if (counter1 != number1 - 1) {
                     counter1++;
                 } else {
                     check1 = 1;
                 }
             } else {
-                minMin = array2[counter2];
+                maxMax = array2[counter2];
                 if (counter2 != number2 - 1) {
                     counter2++;
                 } else {
                     check2 = 1;
                 }
             }
-            array3[i] = minMin;
+            array3[i] = maxMax;
             i++;
         }
         if (check1 == 1) {
